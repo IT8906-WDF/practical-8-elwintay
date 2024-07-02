d90c4f2947efa0f335962f51fd8e8f237186f0ad
elwintay <42425853+elwintay@users.noreply.github.com>
2024-07-02 13:21:21 +0000

    FEAT finished practical 8

--- src/RPSButton.js
+++ src/RPSButton.js
@@ -1,4 +1,6 @@
+import { IsEmojiContext, emoji } from './IsEmojiContext.js'
 export default function RPSButton(props) {
+    const isEmoji = React.useContext(IsEmojiContext);
     const [isHovered, setIsHovered] = React.useState(false);
     const winCount = props.records.filter((record) => record.result === 'Win').length;
     const totalCount = props.records.length;
@@ -9,7 +11,8 @@ export default function RPSButton(props) {
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
-            {props.move} {isHovered && `(${percentage.toFixed(2)}%)`}
+            {isEmoji ? emoji[props.move] : props.move}
+            {isHovered && `(${percentage.toFixed(2)}%)`}
         </button>
     );
 }
--- src/RockPaperScissors.js
+++ src/RockPaperScissors.js
@@ -1,32 +1,34 @@
 import RPSButtons from './RPSButtons.js';
 import RPSInput from './RPSInput.js';
 import RPSRecords from './RPSRecords.js';
+import recordsReducer from './recordsReducer.js';
+import { IsEmojiContext } from './IsEmojiContext.js';
 
 export default function RockPaperScissors(props) {
-    const [records, setRecords] = React.useState([]);
+    const [isEmoji, setIsEmoji] = React.useState(true);
+    // Use Reducer instead
+    const [records, dispatch] = React.useReducer(recordsReducer, []);
     return (
-        <div>
-            <h1>Play rock-paper-scissors with me!</h1>
-            <RPSButtons
-                onButtonPressed={(move) => {
-                    const randomNumber = Math.floor(Math.random() * 3);
-                    let result;
-                    if (randomNumber === 0) result = 'Win';
-                    else if (randomNumber === 1) result = 'Lose';
-                    else result = 'Tie';
-
-                    setRecords([...records, { result: result, move: move }]);
-                }}
-                records={records}
-            />
-            <RPSInput onAdd={(record) => setRecords([...records, record])} />
-            <RPSRecords
-                records={records}
-                onDeleteRecord={(index) => {
-                    records.splice(index, 1);
-                    setRecords([...records]);
-                }}
-            />
-        </div>
+        <IsEmojiContext.Provider value={isEmoji}>
+            <div>
+            <button onClick={function() { setIsEmoji(!isEmoji); }}>Toggle Emoji</button>
+                <RPSButtons
+                    onButtonPressed={(move) => {
+                    // Dispatch instead of setRecords
+                    dispatch({ name: 'add', move: move });
+                    }}
+                    records={records}
+                />
+                // Dispatch instead of setRecords
+                <RPSInput onAdd={(record) => dispatch({ name: 'force add', record: record })} />
+                <RPSRecords
+                    records={records}
+                    onHide={(index) => {
+                    // Dispatch instead of setRecords
+                    dispatch({ name: 'remove', index: index });
+                    }}
+                />
+            </div>
+        </IsEmojiContext.Provider>
     );
-}
+    }
